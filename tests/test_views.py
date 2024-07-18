384a3350815566bab67290697b151cc438affee2
AhmadZakiNYCC <136506725+AhmadZakiNYCC@users.noreply.github.com>
2024-07-18 12:00:08 -0400

    Created unit tests - Setting up test fixture for pytest

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,27 @@
-import pytest
+from django.urls import reverse
 
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    url = reverse('address-parse')
+    response = client.get(url, {'address': address_string})
+    assert response.status_code == 200
+    data = response.json()
+    assert 'address_components' in data
+    assert 'address_type' in data
+    assert 'AddressNumber' in data['address_components']
+    assert 'StreetName' in data['address_components']
+    assert 'PlaceName' in data['address_components']
 
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    url = reverse('address-parse')
+    response = client.get(url, {'address': address_string})
+    assert response.status_code == 400
+    data = response.json()
+    assert 'detail' in data
+    assert (
+        'RepeatedLabelError' in data['detail'] or
+        'Unable to tag this string' in data['detail']
+    )
